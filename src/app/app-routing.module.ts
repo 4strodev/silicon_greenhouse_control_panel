a7addadf7eb65333506a2ac4e40e8e3491bfd4f8
4strodev <jumarurex@gmail.com>
2022-04-30 12:36:26 +0200

    adding first routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsPageComponent } from './pages/events-page/events-page.component';
+import { SensorPageComponent } from './pages/sensor-page/sensor-page.component';
+import { SensorsPageComponent } from './pages/sensors-page/sensors-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sensors', component: SensorsPageComponent },
+  { path: 'sensors/:id', component: SensorPageComponent },
+  { path: 'events', component: EventsPageComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
